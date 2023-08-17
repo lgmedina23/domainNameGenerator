17a206097395bd8b00eef808c919de444362920d
Luis Guillermo <abg.lgmm@gmail.com>
2023-08-17 19:58:33 +0000

    commit1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "his"];
+  let adj = ["great", "big", "amazing"];
+  let noun = ["jogger", "racoon", "fox"];
+  let domain = [".net", ".com", ".org"];
+  let HtmlDomain = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let h = 0; h < noun.length; h++) {
+        for (let t = 0; t < domain.length; t++) {
+          let element =
+            "<li>" + pronoun[i] + adj[j] + noun[h] + domain[t] + "</li>";
+          HtmlDomain += element;
+        }
+      }
+    }
+  }
+
+  let miLista = document.querySelector("#dominios");
+
+  miLista.innerHTML = HtmlDomain;
 };
